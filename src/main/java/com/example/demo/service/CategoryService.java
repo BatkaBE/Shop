9dd0551812ab6a1e7dbcb0b6d54f370216fed0c4
Smoovy <batkabata42@gmail.com>
2025-03-25 09:11:45 +0800

    Засалт : Category

---
+++ src/main/java/com/example/demo/service/CategoryService.java
@@ -0,0 +1,37 @@
+package com.example.demo.service;
+
+import com.example.demo.entity.Category;
+import com.example.demo.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class CategoryService {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    public Category createCategory(Category category) {
+        return categoryRepository.save(category);
+    }
+
+
+    public List<Category> getAllCategories() {
+        return categoryRepository.findAll();
+    }
+
+    public Category getCategoryById(Long id) {
+        return categoryRepository.findById(id).orElseThrow(() -> new RuntimeException("Category not found"));
+    }
+
+    public Category updateCategory(Long id, Category updatedCategory) {
+        Category existingCategory = getCategoryById(id);
+        existingCategory.setName(updatedCategory.getName());
+        return categoryRepository.save(existingCategory);
+    }
+
+    public void deleteCategory(Long id) {
+        categoryRepository.deleteById(id);
+    }
+}
